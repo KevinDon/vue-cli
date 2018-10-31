e4b9c77b857459a9f3fd115249a3e34d2f27a7d5
Kevin Don <kevintang002@gmail.com>
2018-10-31 21:33:06 +0800

    提交Vue cli代码

---
+++ src/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="inputValue">
+    <button v-on:click="handleSubmit">提交</button>
+    <todo-item v-for="(item, index) of list" v-bind:content="item" v-bind:key="index" v-bind:index="index" v-on:delete="handleDelete"></todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  components: {
+    'todo-item': TodoItem
+  },
+  data: function () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  methods: {
+    handleSubmit: function () {
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete: function (index) {
+      this.list.splice(index)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
